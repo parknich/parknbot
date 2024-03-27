f7abe8e3d91a2e45e67a6835875b6448385260c8
parknich081 <parknich082@gmail.com>
2024-03-27 18:53:09 -0400

    test

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-../parknBotVENV/bin/python3 -m flask --app flaskr run --debug --port 80 --host=0.0.0.0 --cert /etc/letsencrypt/live/parknbot.xyz/fullchain.pem --key /etc/letsencrypt/live/parknbot.xyz/privkey.pem
+../parknBotVENV/bin/python3 -m flask --app flaskr run --debug --port 443 --host=0.0.0.0 --cert /etc/letsencrypt/live/parknbot.xyz/fullchain.pem --key /etc/letsencrypt/live/parknbot.xyz/privkey.pem
